3b7690c1ff626a108552331d70dfa8a4d9cee3b3
JustynaTomczyk7 <justyna.toomczyk7@gmail.com>
2023-02-18 19:57:07 +0100

    sassify

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import './mystyle.css';
+
+const hamburger = document.querySelector('#see-menu');
+const menu = document.querySelector('#menu-content');
+
+function seeMenu() {
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    menu.classList.toggle('d-none');
+  });
+}
+
+function onMobileMenuElementClicked() {
+  const menuElements = document.querySelectorAll('#menu li');
+
+  menuElements.forEach(element => {
+    element.addEventListener('click', () => {
+      document.querySelector('.animated-icon').classList.remove('open');
+      hamburger.classList.remove('active');
+      menu.classList.add('d-none');
+    })
+  })
+}
+
+seeMenu();
+onMobileMenuElementClicked();
