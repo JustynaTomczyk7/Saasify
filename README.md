2590586f9d53b9295c2829753ffee023f0e4f630
JustynaTomczyk7 <justyna.toomczyk7@gmail.com>
2023-04-13 17:40:39 +0200

    Add: Prettier, README.md

--- README.md
+++ README.md
@@ -1,15 +1,39 @@
-# 🚀 Welcome to your new awesome project!
+# 🚀 Sassify
 
-This project has been created using **webpack-cli**, you can now run
+## Table of contents
+
+- [General info](#general-info)
+- [Technologies](#technologies)
+- [Installation](#installation)
+- [Illustrations](#illustrations)
+
+## General info
+
+This is my first website that I created after learning HTML and CSS. The site was based on graphics downloaded from www.figma.com and my own modifications.
+
+I decided on this project because it contains a lot of elements for styling, which was an ideal way to test the knowledge I gained in practice.
+
+## Technologies
+
+Project is created with:
+
+- HTML
+- CSS
+- JavaScript
+- Webpack
+
+## Installation
+
+To run this project, install it locally using npm:
 
 ```
-npm run build
+npm install
+npm run serve
 ```
 
-or
+## Illustrations
 
-```
-yarn build
-```
-
-to bundle your application
+![Sassify Image](/src/image/sassify-img1.png)
+![Sassify Image](/src/image/sassify-img2.png)
+![Sassify Image](/src/image/sassify-img3.png)
+![Sassify Image](/src/image/sassify-img4.png)
--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import './mystyle.css';
+import "./mystyle.css";
 
-const hamburger = document.querySelector('#see-menu');
-const menu = document.querySelector('#menu-content');
+const hamburger = document.querySelector("#see-menu");
+const menu = document.querySelector("#menu-content");
 
 function seeMenu() {
-  hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    menu.classList.toggle('d-none');
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("d-none");
   });
 }
 
 function onMobileMenuElementClicked() {
-  const menuElements = document.querySelectorAll('#menu li');
+  const menuElements = document.querySelectorAll("#menu li");
 
-  menuElements.forEach(element => {
-    element.addEventListener('click', () => {
-      document.querySelector('.animated-icon').classList.remove('open');
-      hamburger.classList.remove('active');
-      menu.classList.add('d-none');
-    })
-  })
+  menuElements.forEach((element) => {
+    element.addEventListener("click", () => {
+      document.querySelector(".animated-icon").classList.remove("open");
+      hamburger.classList.remove("active");
+      menu.classList.add("d-none");
+    });
+  });
 }
 
 seeMenu();
